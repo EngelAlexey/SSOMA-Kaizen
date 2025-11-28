47299fc4e4877ecff0dae51448366ef84cd1d2b9
EngelAlexey <alexhmanzanares@gmail.com>
2025-11-28 09:49:33 -0600

    Fixing code

--- web/script.js
+++ web/script.js
@@ -264,7 +264,7 @@ async function sendMessage() {
         threadId: activeSessionId 
     };
 
-    const response = await fetch('http://localhost:3000/chat/query', {
+      const response = await fetch('http://localhost:3000/api/v1/execute', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
